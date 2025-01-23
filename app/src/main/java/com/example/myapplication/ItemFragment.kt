714fd4525b9b36fab97e103cd3f1a697bfa8aac3
danicheman <nostrowski79@gmail.com>
2025-01-23 08:55:53 -0800

    working!

--- app/src/main/java/com/example/myapplication/ItemFragment.kt
+++ app/src/main/java/com/example/myapplication/ItemFragment.kt
@@ -1,15 +1,23 @@
 package com.example.myapplication
 
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
 import android.graphics.Rect
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.DrawableRes
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.myapplication.placeholder.PlaceholderContent
+
 
 /**
  * A fragment representing a list of Items.
@@ -24,6 +32,11 @@ class ItemFragment : Fragment() {
         arguments?.let {
             columnCount = it.getInt(ARG_COLUMN_COUNT)
         }
+    }
+
+    override fun onAttach(context: Context?) {
+        context?.resources
+        super.onAttach(context)
     }
 
     override fun onCreateView(
@@ -47,21 +60,36 @@ class ItemFragment : Fragment() {
     }
 
     class OverlappingItemDecoration : RecyclerView.ItemDecoration() {
+        private var didOnce = false
+
+        //outrect starts as 0,0,0,0
         override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-//            if (parent.getChildAdapterPosition(view) != RecyclerView.NO_POSITION) {
-//                outRect.set(0, 0, 0, - 60)
-//            }
+            //no space for item 5
             if (parent.getChildAdapterPosition(view) == 5) {
-                outRect.set(10, 0, 10, - 60)
+                outRect.set(0, -260, 0, 0)
+            }
+        }
+
+        override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+            super.onDrawOver(c, parent, state)
+
+            if (didOnce) return
+            val prevChild:View = parent.getChildAt(4) ?: return
+            parent.getChildAt(5)?.apply {
+                didOnce = true
+                Log.i(TAG, "onDrawOver: applying to sixth element")
+                x = prevChild.x + 500
+                y = prevChild.y - 50
+                z = 2F
             }
         }
     }
 
     companion object {
+        private const val TAG = "ItemFragment"
 
         // TODO: Customize parameter argument names
         const val ARG_COLUMN_COUNT = "column-count"
-
         // TODO: Customize parameter initialization
         @JvmStatic
         fun newInstance(columnCount: Int) =
